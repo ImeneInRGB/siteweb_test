bab52ff2ce6adf69de72ed753cc234e7f39cf043
FarahInRGB <imene.hessas@gmail.com>
2026-05-15 15:32:47 +0200

    fix: final karma configuration for jenkins success

--- frontend/src/karma.conf.js
+++ frontend/src/karma.conf.js
@@ -1,7 +1,47 @@
-browsers: ['ChromeHeadless'],
-customLaunchers: {
-  ChromeHeadlessCI: {
-    base: 'ChromeHeadless',
-    flags: ['--no-sandbox', '--disable-setuid-sandbox']
-  }
-}
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('jasmine-core'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+        e
+      },
+      clearContext: false
+    },
+    jasmineHtmlReporter: {
+      suppressAllMessages: true 
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, '../coverage/frontend'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false, // Désactivé pour Jenkins
+
+    // --- LA CONFIGURATION CRUCIALE POUR JENKINS / AWS ---
+    browsers: ['ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-setuid-sandbox']
+      }
+    },
+    singleRun: true, // IMPORTANT : Jenkins s'arrête après le test
+    restartOnFileChange: false
+    // ----------------------------------------------------
+  });
+};
